57ea0fb601885d900735d1c190b6310ff27a114d
Thomas <tomtom@MacBook-Pro.attlocal.net>
2021-01-01 17:06:50 -0800

    setup next with tailwind and typescript

---
+++ pages/admin/login.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="text-pink-500">hello world</div>
+    </div>
+  );
+};
+
+export default Home;
